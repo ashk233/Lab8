8381daade1480c8426f8519a707b27d529f51ab6
Ashley Kou <43892820+ashk233@users.noreply.github.com>
2021-02-25 15:02:16 -0800

    Test 3 cases of increasing volumes

--- Part2-Cypress/cypress/integration/party_horn/custom-tests.js
+++ Part2-Cypress/cypress/integration/party_horn/custom-tests.js
@@ -25,7 +25,7 @@ describe('Party Horn Tests', () => {
     cy.get('#volume-slider').invoke('val',33).trigger('input');
     cy.get('#horn-sound').then($el => {
       expect($el).to.have.prop('volume',0.33)
-    })
+    });
   });
 
   it('Image and sound sources change when party horn radio button is selected', () => {
@@ -35,6 +35,39 @@ describe('Party Horn Tests', () => {
     });
     cy.get('#horn-sound').then($el => {
       expect($el).to.have.attr('src','./assets/media/audio/party-horn.mp3')
+    });
+  });
+
+  it('Volume image changes from mute to 1 bar when volume is increased from 0 to 1', () => {
+    cy.get('#volume-slider').invoke('val',0).trigger('input');
+    cy.get('#volume-image').then($el => {
+      expect($el).to.have.attr('src','./assets/media/icons/volume-level-0.svg')
+    });
+    cy.get('#volume-slider').invoke('val',1).trigger('input');
+    cy.get('#volume-image').then($el => {
+      expect($el).to.have.attr('src','./assets/media/icons/volume-level-1.svg')
+    });
+  });
+
+  it('Volume image changes from 1 bar to 2 bars when volume is increased from 1 to 50', () => {
+    cy.get('#volume-slider').invoke('val',1).trigger('input');
+    cy.get('#volume-image').then($el => {
+      expect($el).to.have.attr('src','./assets/media/icons/volume-level-1.svg')
+    });
+    cy.get('#volume-slider').invoke('val',50).trigger('input');
+    cy.get('#volume-image').then($el => {
+      expect($el).to.have.attr('src','./assets/media/icons/volume-level-2.svg')
+    });
+  });
+
+  it('Volume image changes from 2 bars to 3 bars when volume is increased from 50 to 100', () => {
+    cy.get('#volume-slider').invoke('val',50).trigger('input');
+    cy.get('#volume-image').then($el => {
+      expect($el).to.have.attr('src','./assets/media/icons/volume-level-2.svg')
+    });
+    cy.get('#volume-slider').invoke('val',100).trigger('input');
+    cy.get('#volume-image').then($el => {
+      expect($el).to.have.attr('src','./assets/media/icons/volume-level-3.svg')
     });
   });
 });
