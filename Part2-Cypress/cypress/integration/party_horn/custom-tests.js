8c64bbcd31609ffdf27bcad9e7ec9df3ba23b2e2
Ashley Kou <43892820+ashk233@users.noreply.github.com>
2021-02-25 14:43:30 -0800

    Test volume of audio changes when slider changes

--- Part2-Cypress/cypress/integration/party_horn/custom-tests.js
+++ Part2-Cypress/cypress/integration/party_horn/custom-tests.js
@@ -19,5 +19,12 @@ describe('Party Horn Tests', () => {
     cy.get('#volume-number').then($el => {
       expect($el).to.have.value(33);
     })
+  });
+
+  it('Volume of audio changes when slider changes', () => {
+    cy.get('#volume-slider').invoke('val',33).trigger('input');
+    cy.get('#horn-sound').then($el => {
+      expect($el).to.have.prop('volume',0.33)
+    })
   })
 });
